07f0302dc00249bb9f8a303ab9bf2a7fab6898a9
pierreadorni <pierreadorni@free.fr>
2026-05-04 10:17:38 +0200

    move bootstrap logic to separate launcher

--- src/userenv/__main__.py
+++ src/userenv/__main__.py
@@ -1,18 +1,4 @@
-import sys
-import os
-from pathlib import Path
-
-# path to the current script
-script_path = Path(__file__).resolve()
-
-base = script_path.parent.parent
-lib = base / "lib"
-
-# add the lib directory to the system path
-for folder in lib.glob("python*/site-packages"):
-    sys.path.insert(0, str(folder))
+from userenv.cli import app
 
 if __name__ == "__main__":
-    from userenv.cli import app
-
     app()
