451cbec5ae91b6c5ab4f2e8cfd3c4dbe91055e5c
pierreadorni <pierreadorni@free.fr>
2026-05-04 10:09:11 +0200

    infer lib from script location

--- src/userenv/__main__.py
+++ src/userenv/__main__.py
@@ -1,4 +1,18 @@
+import sys
+import os
+from pathlib import Path
+
+# path to the current script
+script_path = Path(__file__).resolve()
+
+base = script_path.parent.parent
+lib = base / "lib"
+
+# add the lib directory to the system path
+for folder in lib.glob("python*/site-packages"):
+    sys.path.insert(0, str(folder))
+
 if __name__ == "__main__":
-    from .cli import app
+    from userenv.cli import app
 
     app()
